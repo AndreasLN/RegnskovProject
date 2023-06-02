11ab15f56e1927b001c4c2b19e918f87134b469d
Andreas Løvsgren Nielsen <75470603+AndreasLN@users.noreply.github.com>
2023-06-02 11:33:34 +0200

    ScriptableObjects igen

--- Regnskov/Assets/Scripts/Dialogue.cs
+++ Regnskov/Assets/Scripts/Dialogue.cs
@@ -8,7 +8,6 @@ using static Dialogue;
 using Random = UnityEngine.Random;
 
 
-<<<<<<< Updated upstream
 [System.Serializable]
 public class DialogueReply
 {
@@ -43,79 +42,6 @@ public class DialogueCollection : MonoBehaviour
 
 }
 
-
-=======
-/*
-
-public class DialogueInstance : MonoBehaviour
-{
-    public String dialogueName;
-
-    public List<Dialogue> dialogues;
-
-    [System.Serializable]
-    public class Dialogues
-    {
-        
-        public List<string> dialogues;
-    }
-
-    [System.Serializable]
-    public class Dialogue
-    {
-        public GameInstance gi;
-        public string dialogueContent;
-        public Sprite face;
-    }
-
-
-    [System.Serializable]
-    public class DialoguesList
-    {
-        public List<Dialogues> dL;
-    }
-
-    public DialoguesList dialoguesList;
-
-    [System.Serializable]
-    public class Faces
-    {
-        public List<Sprite> faces;
-    }
-
-    [System.Serializable]
-    public class FacesList
-    {
-        public List<Faces> fL;
-    }
-
-
-    public FacesList facesList;
-
-    [System.Serializable]
-    public class Replies
-    {
-        public List<Button> replies;
-    }
-
-    [System.Serializable]
-    public class RepliesList
-    {
-        public List<Replies> rL;
-    }
-
-    [System.Serializable]
-    public class RepliesListList
-    {
-        public List<RepliesList> rLL;
-    }
-
-
-    public RepliesListList repliesListList;
-
-}
-*/
->>>>>>> Stashed changes
 public class Dialogue : MonoBehaviour
 {
     //Fields
